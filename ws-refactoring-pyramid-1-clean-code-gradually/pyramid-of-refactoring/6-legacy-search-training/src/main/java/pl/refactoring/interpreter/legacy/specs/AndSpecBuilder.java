64c8613b633947c45df29718c23934751e456949
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-06 14:43:52 +0300

    Deleted unused AndSpecBuilder.withSpec after added alternatives. (var args one)

--- ws-refactoring-pyramid-1-clean-code-gradually/pyramid-of-refactoring/6-legacy-search-training/src/main/java/pl/refactoring/interpreter/legacy/specs/AndSpecBuilder.java
+++ ws-refactoring-pyramid-1-clean-code-gradually/pyramid-of-refactoring/6-legacy-search-training/src/main/java/pl/refactoring/interpreter/legacy/specs/AndSpecBuilder.java
@@ -2,16 +2,10 @@ package pl.refactoring.interpreter.legacy.specs;
 
 import pl.refactoring.interpreter.legacy.Spec;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class AndSpecBuilder {
     private List<Spec> specs;
-
-    public AndSpecBuilder withSpec(Spec... specs) {
-        this.specs = Arrays.asList(specs);
-        return this;
-    }
 
     public AndSpecBuilder withSpec(Spec spec) {
         this.specs.add(spec);
