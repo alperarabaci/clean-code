c19a97d17712759e9259d1f66e5e79be88067ff9
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-06 11:15:50 +0300

    Added AndSpec tests.

---
+++ ws-refactoring-pyramid-1-clean-code-gradually/pyramid-of-refactoring/6-legacy-search-training/src/test/java/pl/refactoring/interpreter/legacy/specs/AndSpecTest.java
@@ -0,0 +1,53 @@
+package pl.refactoring.interpreter.legacy.specs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import pl.refactoring.interpreter.legacy.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AndSpecTest {
+
+    private static final RealEstate BRICK_VILLAGE_BUNGALLOW =
+            new RealEstate(4, 130, EstatePlacement.VILLAGE,
+                    EstateType.BUNGALLOW, EstateMaterial.BRICK);
+
+    Spec yesSpec;
+    Spec noSpec;
+
+    @BeforeEach
+    void setUp() {
+        yesSpec = Mockito.mock(Spec.class);
+        when(yesSpec.isSatisfiedBy(Mockito.any(RealEstate.class))).thenReturn(true);
+
+        noSpec = Mockito.mock(Spec.class);
+        when(noSpec.isSatisfiedBy(Mockito.any(RealEstate.class))).thenReturn(true);
+    }
+
+    @Test
+    public void multipleTrue() {
+        AndSpec andSpec = new AndSpec(yesSpec, yesSpec);
+
+        assertThat(andSpec.isSatisfiedBy(BRICK_VILLAGE_BUNGALLOW))
+                .isTrue();
+
+        Mockito.verify(yesSpec, Mockito.times(2)).isSatisfiedBy(BRICK_VILLAGE_BUNGALLOW);
+    }
+
+    @Test
+    public void multipleFalse() {
+        AndSpec andSpec = new AndSpec(noSpec, noSpec);
+
+        assertThat(andSpec.isSatisfiedBy(BRICK_VILLAGE_BUNGALLOW))
+                .isTrue();
+
+        Mockito.verify(noSpec, Mockito.times(2)).isSatisfiedBy(BRICK_VILLAGE_BUNGALLOW);
+    }
+
+}
